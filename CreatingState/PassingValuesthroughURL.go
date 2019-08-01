286311588828ef589614c9f5eb80a01c97e6e484
saikiranambati942 <saikiranambati942@gmail.com>
2019-08-01 00:10:34 -0700

    PassingvaluesthroughURL

---
+++ CreatingState/PassingValuesthroughURL.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+
+
+
+func main(){
+	http.HandleFunc("/",foo)
+http.ListenAndServe(":8080",nil)
+
+}
+
+
+func foo(w http.ResponseWriter, r *http.Request){
+	v:=r.FormValue("q")
+	io.WriteString(w,"search string is : "+v)
+}
